b9766124c2dee832129dbf62c2229c8a45fbd63b
PriMenesesc <priscila@procomercial.com>
2025-04-14 22:35:49 -0400

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#03FF84",
+        accent: "#7400FF"
+      }
+    }
+  },
+  plugins: []
+};
